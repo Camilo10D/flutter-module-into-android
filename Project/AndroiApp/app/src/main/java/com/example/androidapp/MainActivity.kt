bd1a63c86339786102d356c85570b55a07e49d71
CamiloDiez <diezcamilo10@gmail.com>
2023-10-03 11:38:15 -0500

    Flutter and Kotlin base projects

---
+++ Project/AndroiApp/app/src/main/java/com/example/androidapp/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.androidapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.example.androidapp.ui.theme.AndroidAppTheme
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AndroidAppTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Column(modifier = Modifier.fillMaxSize(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                    ) {
+                        Button(onClick = { goToFlutterModule() }) {
+                            Text("Hello Flutter Module")
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun goToFlutterModule(){
+        startActivity(
+            FlutterActivity.createDefaultIntent(this)
+        )
+    }
+}
+
